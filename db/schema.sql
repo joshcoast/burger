e0bff265e4af74fc5f75e3af36d5dd67d417d352
Josh Coast <josh@joshcoast.com>
2018-05-13 22:24:07 -0600

    Add schema notes

--- db/schema.sql
+++ db/schema.sql
@@ -10,3 +10,17 @@ CREATE TABLE burgers
 	devoured BOOLEAN DEFAULT false,
 	PRIMARY KEY (id)
 );
+
+### heroku Schema? We don't use the CREATE DATABASE or the USE
+
+CREATE TABLE burgers
+(
+	id int NOT NULL AUTO_INCREMENT,
+	burger_name varchar(255) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
+);
+
+
+
+  ### alos, I don't think we need this. createdAt TIMESTAMP NOT NULL,
